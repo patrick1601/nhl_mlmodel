c94fa74734302f402a5e5a2fcdd13d9f90ed782a
Patrick Petanca <ppetanca@gmail.com>
2021-01-13 10:59:28 -0700

    created folder structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nhl_mlmodel',
+    version='0.0.1',
+    packages=['nhl_mlmodel', 'nhl_mlmodel.covers_scraper'],
+    url='https://github.com/patrick1601/nhl_mlmodel',
+    license='MIT License 2021',
+    author='Patrick Petanca',
+    author_email='ppetanca@gmail.com',
+    description='XGBoost NHL Model'
+)
